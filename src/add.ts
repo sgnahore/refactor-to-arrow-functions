67f5452ac8be3fb48bcfa354cd49471c2f9609e1
Sgnahore <stephaniergnahore@gmail.com>
2023-07-03 15:11:14 +0100

    full body arrow functions

--- src/add.ts
+++ src/add.ts
@@ -5,9 +5,14 @@
  * @param secondNum second number to add
  * @returns total of the two numbers
  */
-function add(firstNum: number, secondNum: number): number {
+// function add(firstNum: number, secondNum: number): number {
+//   const total = firstNum + secondNum;
+//   return total;
+// }
+
+const add = (firstNum: number, secondNum: number): number => {
   const total = firstNum + secondNum;
   return total;
-}
+};
 
 export default add;
